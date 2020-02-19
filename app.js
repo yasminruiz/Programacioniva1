94d7af9a3f313a21c6d27e0dd2652233d03ea790
ArgueraDev <robertoarguera@gmail.com>
2020-02-19 13:04:58 -0600

    correcciones

--- app.js
+++ app.js
@@ -4,17 +4,17 @@ document.addEventListener("DOMContentLoaded", e=>{
       event.preventDefault();
 
       let de = document.querySelector("#cboDe").value,
-      a = document.querySelector("cboA").value,
-      cantidad = document.querySelector("txtCantidadConversor").value;
-      console.log(de,a,cantidad);
+          a = document.querySelector("#cboA").value,
+          cantidad = document.querySelector("#txtCantidadConversor").value;
+      console.log(de, a, cantidad);
       let monedas = {
-          "dolar": 1,
-          "euro": 0.93,
-          "quetzal": 7.63,
-          "lempira": 24.9,
-          "cordoba": 34.19
-        };
+          "dolar":1,
+          "euro":0.93,
+          "quetzal":7.63,
+          "lempira":24.9,
+          "cordoba":34.19
+      };
+      let $res = document.querySelector("#lblRespuesta");
+      $res.innerHTML = `Respuesta: ${ (monedas[a]/monedas[de]*cantidad).toFixed(2) }`;
   });
-  let $res = document.querySelector("#lblRespuesta");
-  $res.innerHTML = `respuesta: ${ (monedas[a]/monedas[de]* cantidad).toFixed(2)}`;
 });
