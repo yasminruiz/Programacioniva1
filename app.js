35c40a167a8780bb793ea47594ab02c0e282b9da
ArgueraDev <robertoarguera@gmail.com>
2020-02-19 20:33:34 -0600

    se agregaron mas conversores

--- app.js
+++ app.js
@@ -6,15 +6,70 @@ document.addEventListener("DOMContentLoaded", e=>{
       let de = document.querySelector("#cboDe").value,
           a = document.querySelector("#cboA").value,
           cantidad = document.querySelector("#txtCantidadConversor").value;
-      console.log(de, a, cantidad);
+
       let monedas = {
           "dolar":1,
           "euro":0.93,
           "quetzal":7.63,
           "lempira":24.9,
-          "cordoba":34.19
-      };
+          "cordoba":34.19},
+          longitudes = {
+            "mm": 1000,
+            "cm": 100,
+            "mt": 1,
+            "km": 0.001,
+            "milla": 0.000621371},
+            almacenamiento = {
+              "bit": 8,
+              "byte": 1,
+              "kb": 0.001,
+              "mb": 0.000001,
+              "gb":0.00000000093132},
+              peso = {
+                "g": 1000,
+                "kg": 1,
+                "libra": 2.20462,
+                "onza": 35.274,
+                "tonelada": 0.01};
+
       let $res = document.querySelector("#lblRespuesta");
       $res.innerHTML = `Respuesta: ${ (monedas[a]/monedas[de]*cantidad).toFixed(2) }`;
   });
 });
+
+//llenar los select box 
+function elegir_opcion() {
+  let opcion = document.getElementById('cboConvertir'),
+  de1 = document.getElementById('cboDe'),
+  a1 = document.getElementById('cboA');
+  //limpia antes de actualizar
+  de1.value="";
+  a1.value="";
+  de1.innerHTML="";
+  a1.innerHTML="";
+
+  if(opcion.value == "moneda"){
+    var  array = ["dolar!Dolar","euro!Euro","quetzal!Quetzal","lempira!Lempira","cordoba!Cordoba"]; 
+  } else if(opcion.value == "longitud"){
+    var array = ["mm!MM","cm!CM","mt!MT","km!KM","milla!Milla"];
+  } else if(opcion.value == "almacenamiento"){
+    var array = ["bit!Bit","byte!Byte","kb!KB","mb!MB","gb!GB"];
+  } else if(opcion.value == "peso"){
+    var array = ["gramo!Gramo","kg!KG","libra!Libra","onza!Onza","tonelada!Tonelada"];
+  };
+
+  for(var i=0;i<array.length;i++){ 
+    var repair = array[i].split("!");
+    var newop = document.createElement("option");
+    newop.value = repair[0]
+    newop.innerHTML = repair[1];
+    de1.options.add(newop);
+  };
+  for(var i=0;i<array.length;i++){ 
+    var repair = array[i].split("!");
+    var newop = document.createElement("option");
+    newop.value = repair[0]
+    newop.innerHTML = repair[1];
+    a1.options.add(newop);
+  };
+ }
