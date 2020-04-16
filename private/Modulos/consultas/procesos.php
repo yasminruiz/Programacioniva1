a21e8e50ba62010e7aaf4aae094aac7cae4275d6
ArgueraDev <robertoarguera@gmail.com>
2020-04-16 09:46:46 -0600

    creacion de formularios

---
+++ private/Modulos/consultas/procesos.php
@@ -0,0 +1,46 @@
+<?php 
+include('../../Config/Config.php');
+$consulta = new consulta($conexion);
+
+$proceso = '';
+if( isset($_GET['proceso']) && strlen($_GET['proceso'])>0 ){
+	$proceso = $_GET['proceso'];
+}
+$consulta->$proceso( $_GET['consulta'] );
+print_r(json_encode($consulta->respuesta));
+
+class consulta{
+    private $datos = array(), $db;
+    public $respuesta = ['msg'=>'correcto'];
+
+    public function __construct($db){
+        $this->db=$db;
+    }
+    public function recibirDatos($consulta){
+        $this->datos = json_decode($consulta, true);
+        $this->validar_datos();
+    }
+    private function validar_datos(){
+        if( empty($this->datos['nombre']) ){
+            $this->respuesta['msg'] = 'por favor ingrese su nombre';
+        }
+        if( empty($this->datos['consulta']) ){
+            $this->respuesta['msg'] = 'por favor ingrese la consulta';
+        }
+        $this->almacenar_consulta();
+    }
+    private function almacenar_consulta(){
+        if( $this->respuesta['msg']==='correcto' ){
+            if( $this->datos['accion']==='nuevo' ){
+                $this->db->consultas('
+                    INSERT INTO consultas (nombre,consulta) VALUES(
+                        "'. $this->datos['nombre'] .'",
+                        "'. $this->datos['consulta'] .'"
+                    )
+                ');
+                $this->respuesta['msg'] = 'Registro insertado correctamente';
+            } 
+        }
+    }
+}
+?>
---
+++ public/vistas/consultas/consultas.js
@@ -0,0 +1,24 @@
+var appconsulta = new Vue({
+  el:'#frm-consultas',
+  data:{
+      consulta:{
+          idConsulta  : 0,
+          accion    : 'nuevo',
+          nombre    : '',
+          consulta : '',
+          msg       : ''
+      }
+  },
+  methods:{
+      guardarConsulta:function(){
+          fetch(`private/Modulos/consultas/procesos.php?proceso=recibirDatos&consulta=${JSON.stringify(this.consulta)}`).then( resp=>resp.json() ).then(resp=>{
+              this.consulta.msg = resp.msg;
+              this.consulta.idConsulta = 0;
+              this.consulta.nombre = '';
+              this.consulta.consulta = '';
+              this.consulta.accion = 'nuevo';
+              appBuscarConsulta.buscarConsulta();
+          });
+      }
+    }
+});
