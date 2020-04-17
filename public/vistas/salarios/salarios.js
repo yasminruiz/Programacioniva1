09c738963f2649463e65c052778b0597a68ded35
ArgueraDev <robertoarguera@gmail.com>
2020-04-17 09:49:37 -0600

    ...

--- public/vistas/salarios/salarios.js
+++ public/vistas/salarios/salarios.js
@@ -1,24 +1,14 @@
-var appconsulta = new Vue({
-  el:'#frm-consultas',
+var appsalario = new Vue({
+  el:'#frm-salarios',
   data:{
-      consulta:{
-          idConsulta  : 0,
-          accion    : 'nuevo',
-          nombre    : '',
-          consulta : '',
+      salario:{
+          cantidad    : '',
           msg       : ''
       }
   },
   methods:{
-      guardarConsulta:function(){
-          fetch(`private/Modulos/consultas/procesos.php?proceso=recibirDatos&consulta=${JSON.stringify(this.consulta)}`).then( resp=>resp.json() ).then(resp=>{
-              this.consulta.msg = resp.msg;
-              this.consulta.idConsulta = 0;
-              this.consulta.nombre = '';
-              this.consulta.consulta = '';
-              this.consulta.accion = 'nuevo';
-              appBuscarConsulta.buscarConsulta();
-          });
+      CalcularSalario:function(){
+        this.salario.msg = "salario.cantidad" ;
       }
     }
 });
