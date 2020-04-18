9635a8a785af2eda7e0fb0d3dca11202a0cbbc76
ArgueraDev <robertoarguera@gmail.com>
2020-04-18 11:52:07 -0600

    avance en el funcionamiento de calculo de salario

--- public/vistas/salarios/salarios.js
+++ public/vistas/salarios/salarios.js
@@ -2,13 +2,34 @@ var appsalario = new Vue({
   el:'#frm-salarios',
   data:{
       salario:{
-          cantidad    : '',
-          msg         : ''
+          cantidad  : '',
+          isss      : '',
+          afp       : '',
+          d2        : '',
+          msg       : ''
       }
   },
   methods:{
-      CalcularSalario:function(){
-        this.salario.msg = "hola" ;
+    CalcularSalario:function(){
+      var dato = this.salario.cantidad;
+      var ISSS, AFP,D2, ISR, D3;
+      if(dato<=1000){
+        ISSS = (dato * 0.03).toFixed(2);
+      } else if(dato>1000){
+        ISSS = 30
       }
+
+      if(dato<=10000){
+        AFP = (dato * 0.07).toFixed(2);
+      } else if(dato>10000){
+        AFP = 30
+      }
+
+      D2 = dato - ISSS - AFP;
+
+      this.salario.isss = "ISSS: $" + ISSS;
+      this.salario.afp = "AFP: $" + AFP;
+      this.salario.d2 ="Salario menos ISSS y AFP: $"+ D2 ;
     }
+  }
 });
