bccb51c5331787418242f581802adff012fd339b
ArgueraDev <robertoarguera@gmail.com>
2020-04-19 17:01:06 -0600

    ...

--- public/vistas/cuotas/cuotas.js
+++ public/vistas/cuotas/cuotas.js
@@ -2,13 +2,22 @@ var appcuota = new Vue({
   el:'#frm-cuotas',
   data:{
       cuota:{
-          cantidad  : '',
-          msg       : ''
+        cantidad     : '',
+        bancos       : '',
+        prestamo     : '',
+        msg          : ''
+      },
+      datos:{
+        bancos:['Banco Agricola','BFA'],
+        Agricola:{prestamos:['Crédito de Consumo','Crédito de Vivienda','Crédito de Estudio']},
+        BFA:{prestamos:['Crédito de Consumo','Crédito de Vivienda']}
       }
   },
   methods:{
       calcularCuota:function(){
         this.cuota.msg = "hola" ;
+      },
+      llenarLista(){
       }
-    }
+  }
 });
